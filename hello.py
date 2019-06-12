175b08481e5cb10fc8b50f0be7a2ef81ca60955e
mshero7 <mshero7@github.com>
2019-06-12 13:10:11 +0900

    first python Project from pycharm

---
+++ hello.py
@@ -0,0 +1,26 @@
+print('hello3')
+
+
+def add(m, n):
+    s = m
+    s += n
+    return s
+
+
+def mymax(m, n):
+    if m > n:
+        return m
+    else:
+        return n
+
+
+a = 10
+b = 1
+
+if a > 1:
+    print('hello world')
+    print('hello world')
+    print('hello world')
+
+print('end')
+
